4907a7476637eebf82ea0842828e82c29b45af75
Karol Kropisz <karolkropisz@gmail.com>
2024-01-13 12:19:16 +0100

    Add page NotFound

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import Home from "./Home/Home.js";
 import Favorite from "./components/Favorite/Favorite.js";
 import About from "./components/About/About.js";
+import NotFound from "./components/NotFound/NotFound.js";
 import Contrainer from "./components/Contrainer/Contrainer";
 import NavBar from "./components/NavBar/NavBar";
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <main>
-     <NavBar />
+<main>
+      <NavBar />
       <Contrainer>
-        <Home />
-        <About />
-        <Favorite />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Contrainer>
-    </main>
+ </main>
   );
 };
 
